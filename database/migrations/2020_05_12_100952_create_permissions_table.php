05524cd18f20d9e82556798f8c14a45b261ebe0e
Nazam <nazam98-be@email.com>
2020-05-12 12:26:29 +0200

    corrected permission

--- database/migrations/2020_05_12_100952_create_permissions_table.php
+++ database/migrations/2020_05_12_100952_create_permissions_table.php
@@ -15,7 +15,7 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
             $table->timestamps();
         });
     }
