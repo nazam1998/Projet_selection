c43ec559a2485c9c28ed7fa0c81ccf78aed02b53
Nazam <nazam98-be@email.com>
2020-05-13 18:33:36 +0200

    test

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,27 +2,16 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-use App\Annonce;
-use App\Evenement;
-=======
 // use App\Annonce;
 // use App\Evenement;
->>>>>>> badGit
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
-        $evenements = Evenement::latest();
-        $annonce = Annonce::first();
-        return view('welcome', compact('annonce'));
-=======
         // $evenements = Evenement::latest();
         // $annonce = Annonce::first();
         return view('welcome');
->>>>>>> badGit
     }
 }
