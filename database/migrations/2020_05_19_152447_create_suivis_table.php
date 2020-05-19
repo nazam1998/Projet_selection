a79899303733117093c41350e4ea114d51ecc10c
Nazam <nazam98-be@email.com>
2020-05-19 16:02:22 +0200

    ajout message lorsque plus event  en vours

--- database/migrations/2020_05_19_152447_create_suivis_table.php
+++ database/migrations/2020_05_19_152447_create_suivis_table.php
@@ -15,6 +15,11 @@ class CreateSuivisTable extends Migration
     {
         Schema::create('suivis', function (Blueprint $table) {
             $table->id();
+            $table->boolean('ecriture');
+            $table->unsignedBigInteger('auth_id');
+            $table->foreign('auth_id')->on('roles')->references('id')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->on('roles')->references('id')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
