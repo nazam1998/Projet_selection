159b7e5ec857a9eac4d74ce4402b801862b28a26
Nazam <nazam98-be@email.com>
2020-06-08 18:14:22 +0200

    changed app env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\App;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -26,5 +26,11 @@ class AppServiceProvider extends ServiceProvider
     {
         setlocale(LC_TIME, 'fr_FR.UTF-8');
         Carbon::setLocale(config('app.locale'));
+
+        if (App::environment('prod')) {
+            $this->app->bind('path.public', function () {
+                return base_path('../../public_html');
+            });
+        }
     }
 }
