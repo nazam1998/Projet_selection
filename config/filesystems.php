c286a9dd7770f8011cc16862cc3dd96b25377ee6
Nazam <nazam98-be@email.com>
2020-06-08 20:12:20 +0200

    changed storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path().'/storage',
+            'root' => env('APP_URL').'/storage',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
